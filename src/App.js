bb3dd6081ce14827149a065940a28d4475b26240
sairu-sh <tmg.sairush@gmail.com>
2024-02-04 16:49:16 +0545

    created a state variable step with useState

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
@@ -5,7 +7,15 @@ const messages = [
 ];
 
 export default function App() {
-  const step = 1;
+  const [step, setStep] = useState(1);
+  function handlePrevious() {
+    if (step > 1) console.log("prev");
+  }
+  function handleNext() {
+    if (step < 3) console.log("next");
+
+    console.log(step);
+  }
   return (
     <div className="steps">
       <div className="numbers">
@@ -19,10 +29,16 @@ export default function App() {
       </p>
 
       <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
           Previous
         </button>
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
           Next
         </button>
       </div>
