ac285fc96a73c1d54ade24e2a533cf892b40dcc1
sairu-sh <tmg.sairush@gmail.com>
2024-02-04 16:18:48 +0545

    finished the basic template for the steps

---
+++ src/App.js
@@ -0,0 +1,31 @@
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  const step = 1;
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className="active">1</div>
+        <div className="number">2</div>
+        <div className="number">3</div>
+      </div>
+
+      <p className="message">
+        Step{step}: {messages[step]}
+      </p>
+
+      <div className="buttons">
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
